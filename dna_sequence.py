70a23fbe3dc3cd7dc410b24cdee2dfa3ca06b9f0
Coppini <25513836+Coppini@users.noreply.github.com>
2024-05-31 18:29:25 -0300

    refactor: moves the Encoding to the code_mapping file and properly imports things

--- dna_sequence.py
+++ dna_sequence.py
@@ -3,35 +3,14 @@ from enum import IntEnum
 from math import ceil, sqrt
 from typing import Iterable, NamedTuple, Optional
 
-# Bases
-REGULAR_BASES = set("ATCG")
-NS_AND_GAPS = set("N-")
-DEGENERATE_BASES = set("RYMKSWHBVD")
-
-
-class Encoding(IntEnum):
-    BIT2_ATCG = 2
-    BIT3_Ns_and_GAPs = 3
-    BIT4_FULL_IUPAC = 4
-
-
-ENCODING_TO_BASES = {
-    Encoding.BIT2_ATCG: REGULAR_BASES,
-    Encoding.BIT3_Ns_and_GAPs: REGULAR_BASES.union(NS_AND_GAPS),  # E
-    Encoding.BIT4_FULL_IUPAC: REGULAR_BASES.union(NS_AND_GAPS).union(DEGENERATE_BASES),
-}
-
-# Mapping dictionaries
-ENCODE_MAPPING = {
-    encoding_type: {
-        base: format(i, f"0{encoding_type}b") for i, base in enumerate(bases)
-    }
-    for encoding_type, bases in ENCODING_TO_BASES.items()
-}
-DECODE_MAPPING = {
-    encoding_type: {value: key for key, value in mapping.items()}
-    for encoding_type, mapping in ENCODE_MAPPING.items()
-}
+from code_mapping import (
+    DECODE_MAPPING,
+    DEGENERATE_BASES,
+    ENCODE_MAPPING,
+    ENCODING_TO_BASES,
+    NS_AND_GAPS,
+    Encoding,
+)
 
 
 class EncodedSequence(NamedTuple):
