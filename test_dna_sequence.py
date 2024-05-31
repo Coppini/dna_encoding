1aee12b5fee55c1b7aba5daf0420d32b5c9d5165
Coppini <25513836+Coppini@users.noreply.github.com>
2024-05-31 19:08:59 -0300

    test: add test for headers

--- test_dna_sequence.py
+++ test_dna_sequence.py
@@ -1,8 +1,9 @@
 import random
+import string
 
 import pytest
 
-from dna_sequence import ENCODING_TO_BASES, DNASequence, EncodedSequence, Encoding
+from dna_sequence import ENCODING_TO_BASES, DNASequence, Encoding
 
 
 @pytest.mark.parametrize(
@@ -35,15 +36,19 @@ def test_encode_quality_score_length_mismatch():
 
 
 @pytest.mark.parametrize("encoding_type", list(Encoding))
-def test_encode_decode_random_fasta(encoding_type):
+@pytest.mark.parametrize("header", [True, False])
+def test_encode_decode_random_fasta(encoding_type: Encoding, header: bool):
     # Arrange
     length = random.randint(10, 100)
     available_bases = sorted(ENCODING_TO_BASES[encoding_type])
     dna_sequence = "".join(random.choice(available_bases) for _ in range(length))
+    seq_header = random.choice(string.ascii_letters) if header else None
     quality = None
 
     # Act
-    encoded_dna = DNASequence.from_sequence(dna_sequence)
+    encoded_dna = DNASequence.from_sequence(
+        dna_sequence=dna_sequence, header=seq_header
+    )
 
     # Assert
     assert encoded_dna.encoded_sequence.encoding_type == encoding_type
@@ -53,7 +58,10 @@ def test_encode_decode_random_fasta(encoding_type):
     assert isinstance(encoded_dna.average_quality, float)
     assert not any([encoded_dna.average_quality <= 0, encoded_dna.average_quality >= 0])
     fasta_header, fasta_sequence = encoded_dna.fasta.split("\n")
-    assert fasta_header.startswith(">")
+    if header:
+        assert fasta_header == f">{seq_header}"
+    else:
+        assert fasta_header.startswith(">")
     assert fasta_sequence == dna_sequence
     with pytest.raises(ValueError):
         encoded_dna.fastq
@@ -65,8 +73,9 @@ def test_encode_decode_random_fasta(encoding_type):
     "quality2", [0, 1, 2, 3, 4, 7, 8, 15, 16, 31, 32, 63, 64, 127, 128, 255]
 )
 @pytest.mark.parametrize("sort", [True, False])
+@pytest.mark.parametrize("header", [True, False])
 def test_encode_decode_random_fastq(
-    encoding_type: Encoding, quality1: int, quality2: int, sort: bool
+    encoding_type: Encoding, quality1: int, quality2: int, sort: bool, header: bool
 ):
     # Arrange
     length = random.randint(100, 1000)
@@ -74,6 +83,7 @@ def test_encode_decode_random_fastq(
     dna_sequence = "".join(random.choice(available_bases) for _ in range(length))
     min_quality = min([quality1, quality2])
     max_quality = max([quality1, quality2])
+    seq_header = random.choice(string.ascii_letters) if header else None
     quality_scores = (
         [min_quality]
         + [random.randint(min_quality, max_quality) for _ in range(length - 2)]
@@ -87,7 +97,9 @@ def test_encode_decode_random_fastq(
     quality = "".join(chr(score + 33) for score in quality_scores)
 
     # Act
-    encoded_dna = DNASequence.from_sequence(dna_sequence, quality)
+    encoded_dna = DNASequence.from_sequence(
+        dna_sequence=dna_sequence, quality_score_str=quality, header=seq_header
+    )
 
     # Assert
     assert encoded_dna.encoded_sequence.encoding_type == encoding_type
@@ -95,15 +107,19 @@ def test_encode_decode_random_fastq(
     assert encoded_dna.quality == quality
     assert encoded_dna.average_quality == average_quality
     fasta_header, fasta_sequence = encoded_dna.fasta.split("\n")
-    assert fasta_header.startswith(">")
     assert fasta_sequence == dna_sequence
     fastq_header, fastq_sequence, plus_signal, fastq_quality = encoded_dna.fastq.split(
         "\n"
     )
-    assert fastq_header.startswith("@")
     assert fastq_sequence == dna_sequence
     assert plus_signal == "+"
     assert fastq_quality == quality
+    if header:
+        assert fasta_header == f">{seq_header}"
+        assert fastq_header == f"@{seq_header}"
+    else:
+        assert fasta_header.startswith(">")
+        assert fastq_header.startswith("@")
 
 
 if __name__ == "__main__":
